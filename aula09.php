6e2c948434fb43da87df6e7eaa1f715d94486a1e
Camila Fernanda <163914073+camilafernandals@users.noreply.github.com>
2024-08-16 16:36:23 -0300

    aula09.php

--- aula09.php
+++ aula09.php
@@ -8,11 +8,11 @@
 <body>
     <form action="calcular.php" method="post">
         <label for="n1">Número 1: </label>
-        <input type="text" nome="n1"> <br>
+        <input type="text" name="n1"> <br>
         <label for="n2">Número 2: </label>
         <input type="text" name="n2"> <br>
         <input type="submit" value="Somar"> <br>
-        Resultado: <input type="text" width="5"> <br>
+        Resultado: <input type="text" width="5" value="<?php echo (isset($_GET["res"])?$_GET["res"]:"");?>"> <br>
     </form>
 </body>
 </html>
